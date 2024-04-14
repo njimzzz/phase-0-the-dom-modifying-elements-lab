2b22bc516550a93415de4deddcfe1f5193b24668
Kyle <kylejrgurlad@gmail.com>
2024-04-14 16:15:51 +0300

    completed task

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+// document.querySelector(#main) {
+
+// }
+main.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "YOUR-NAME is the champion"
